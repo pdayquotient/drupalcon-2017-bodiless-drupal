e7e8d960c2c214a4b5a0db3582cfb0c7f14c202c
rbartlettquotient <rbartlett@quotient-inc.com>
2017-04-23 12:54:19 -0400

    Clean up the headless standalone script.

--- headless.php
+++ headless.php
@@ -5,14 +5,12 @@
 
 try {
   // Get the sample file contents through the Drupal route
-
   $headless_route = $_SERVER['HTTP_HOST'] . '/headless';
 
   $ch = curl_init();
   curl_setopt($ch, CURLOPT_URL, $headless_route);
 
   curl_setopt($ch, CURLOPT_HEADER, 0);
-//  curl_setopt($ch, CURLOPT_HTTPHEADER, $this->encodeHeader($uri));
   curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
   curl_setopt($ch, CURLINFO_HEADER_OUT, 1);
 
@@ -25,19 +23,12 @@ try {
   }
   else {
     if($info['http_code'] == 403) {
-      print("Access denied! Make sure the user role has access to this route. The permission to allow is 'view headless content'.");
+      echo("Access denied! Make sure the user role has access to this route. The permission to allow is 'view headless content'.");
     }
     else {
-      print("Something went wrong! We didn't get a 200 HTTP code, we got: " . $info['http_code']);
+      echo("Something went wrong! We didn't get a 200 HTTP code, we got: " . $info['http_code']);
     }
   }
-
-
-  die();
-//  $data = file_get_contents('http://bodiless.dd:8083/headless');
-
-  // Write them out to the browser!
-//  echo $data;
 
 }
 catch(Exception $ex) {
